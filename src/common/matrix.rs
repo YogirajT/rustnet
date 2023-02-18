f44b0b868a2d5981273fee4bb16cba03426b724f
YogirajT <tawdeyogiraj@yahoo.com>
2023-02-18 23:48:45 +0530

    WIP: back prop

--- src/common/matrix.rs
+++ src/common/matrix.rs
@@ -1,12 +1,16 @@
+#![allow(dead_code)]
+use super::types::NetworkParams;
 use csv::Reader;
 use rand::{Rng, SeedableRng};
 use rand_pcg::Pcg64;
 use std::clone::Clone;
 use std::io::Stdin;
 
-use super::types::NetworkParams;
+pub enum Operation {
+    SUBTRACT,
+    ADD,
+}
 
-#[allow(dead_code)]
 pub fn dot_product(matrix_1: &Vec<Vec<f64>>, matrix_2: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
     let m1_rows = matrix_1.len();
     let m1_cols = matrix_1[0].len();
@@ -29,7 +33,6 @@ pub fn dot_product(matrix_1: &Vec<Vec<f64>>, matrix_2: &Vec<Vec<f64>>) -> Vec<Ve
     result
 }
 
-#[allow(dead_code)]
 pub fn create_vec_from_csv(mut rdr: Reader<Stdin>) -> Vec<Vec<f64>> {
     let mut vec = vec![];
     for result in rdr.records() {
@@ -45,7 +48,6 @@ pub fn create_vec_from_csv(mut rdr: Reader<Stdin>) -> Vec<Vec<f64>> {
     vec
 }
 
-#[allow(dead_code)]
 pub fn transpose<T: Clone>(matrix: &[Vec<T>]) -> Vec<Vec<T>> {
     let row_count = matrix.len();
     let column_count = matrix[0].len();
@@ -61,7 +63,6 @@ pub fn transpose<T: Clone>(matrix: &[Vec<T>]) -> Vec<Vec<T>> {
     transposed
 }
 
-#[allow(dead_code)]
 pub fn shuffle_matrix<T>(matrix: &mut [Vec<T>]) {
     let mut rng = rand::thread_rng();
     shuffle(matrix, &mut rng);
@@ -77,13 +78,11 @@ fn shuffle<T, R: Rng>(matrix: &mut [Vec<T>], rng: &mut R) {
     }
 }
 
-#[allow(dead_code)]
 pub fn split_matrix(matrix: &[Vec<f64>], n: usize) -> (Vec<Vec<f64>>, Vec<Vec<f64>>) {
     let (first_n_rows, last_n_rows) = matrix.split_at(n);
     (first_n_rows.to_vec(), last_n_rows.to_vec())
 }
 
-#[allow(dead_code)]
 pub fn get_nth_column<T>(matrix: &[Vec<T>], n: usize) -> Vec<T>
 where
     T: Clone,
@@ -91,7 +90,6 @@ where
     matrix.iter().map(|row| row[n].clone()).collect()
 }
 
-#[allow(dead_code)]
 pub fn rand_matrix(rows: usize, columns: usize) -> Vec<Vec<f64>> {
     let mut rng = Pcg64::seed_from_u64(0);
     let mut result = Vec::new();
@@ -110,7 +108,6 @@ pub fn rand_matrix(rows: usize, columns: usize) -> Vec<Vec<f64>> {
     result
 }
 
-#[allow(dead_code)]
 pub fn get_network_params() -> NetworkParams {
     let w_1 = rand_matrix(10, 784);
     let b_1 = rand_matrix(10, 1);
@@ -120,9 +117,33 @@ pub fn get_network_params() -> NetworkParams {
     (w_1, b_1, w_2, b_2)
 }
 
-#[allow(dead_code)]
-pub fn matrix_addition(matrix: &[Vec<f64>], bias: &[Vec<f64>]) -> Vec<Vec<f64>> {
-    let bias_first_col = bias.last().unwrap();
+pub fn linear_op(action: Operation, matrix: &[Vec<f64>], bias: &[Vec<f64>]) -> Vec<Vec<f64>> {
+    let bias_first_col = match bias {
+        [x] => x,
+        _ => panic!("expected single element"),
+    };
+    matrix
+        .iter()
+        .map(|row| {
+            row.iter()
+                .zip(bias_first_col.iter())
+                .map(|(&m, &v)| {
+                    if matches!(action, Operation::SUBTRACT) {
+                        m - v
+                    } else {
+                        m + v
+                    }
+                })
+                .collect()
+        })
+        .collect()
+}
+
+pub fn one_hot(matrix: &[Vec<f64>], bias: &[Vec<f64>]) -> Vec<Vec<f64>> {
+    let bias_first_col = match bias {
+        [x] => x,
+        _ => panic!("expected single element"),
+    };
     matrix
         .iter()
         .map(|row| {
@@ -132,4 +153,8 @@ pub fn matrix_addition(matrix: &[Vec<f64>], bias: &[Vec<f64>]) -> Vec<Vec<f64>> 
                 .collect()
         })
         .collect()
+}
+
+pub fn zeroes(rows: usize, cols: usize) -> Vec<Vec<f64>> {
+    vec![vec![0.0; cols]; rows]
 }
--- src/common/network_functions.rs
+++ src/common/network_functions.rs
@@ -1,9 +1,11 @@
+#![allow(dead_code)]
+use super::matrix::Operation::{ADD, SUBTRACT};
+
 use super::{
-    matrix::{dot_product, matrix_addition},
+    matrix::{dot_product, linear_op, transpose, zeroes},
     types::NetworkParams,
 };
 
-#[allow(dead_code)]
 fn relu(input: &[Vec<f64>]) -> Vec<Vec<f64>> {
     let row_count = input.len();
     let column_count = input.first().unwrap().len();
@@ -18,7 +20,20 @@ fn relu(input: &[Vec<f64>]) -> Vec<Vec<f64>> {
     output
 }
 
-#[allow(dead_code)]
+fn relu_derivative(input: &[Vec<f64>]) -> Vec<Vec<f64>> {
+    let row_count = input.len();
+    let column_count = input.first().unwrap().len();
+    let mut output = vec![vec![0.0; row_count]; column_count];
+
+    for (i, row) in input.iter().enumerate().take(row_count) {
+        for (j, cell) in row.iter().enumerate().take(column_count) {
+            output[i][j] = if *cell > 0.0 { *cell } else { 0.0 }
+        }
+    }
+
+    output
+}
+
 pub fn softmax(input: &[Vec<f64>]) -> Vec<Vec<f64>> {
     let row_count = input.len();
     let column_count = input.first().unwrap().len();
@@ -37,7 +52,27 @@ pub fn softmax(input: &[Vec<f64>]) -> Vec<Vec<f64>> {
     output
 }
 
-#[allow(dead_code)]
+pub fn transform_labels_to_network_output(labels: &[Vec<f64>]) -> Vec<Vec<f64>> {
+    let labels_first_col = match labels {
+        [x] => x,
+        _ => panic!("expected single element"),
+    };
+
+    let usize_col: Vec<usize> = labels_first_col.iter().map(|x| *x as usize).collect();
+    let rows_len = labels_first_col.len();
+    let cols_len = usize_col.iter().max().unwrap();
+
+    let mut zeroes_matrix = zeroes(rows_len, *cols_len + 1);
+
+    for i in 0..rows_len {
+        let label = usize_col[i];
+
+        zeroes_matrix[i][label] = 1.0;
+    }
+
+    transpose::<f64>(&zeroes_matrix)
+}
+
 pub fn forward_propagation(
     network_params: NetworkParams,
     input_image: Vec<Vec<f64>>,
@@ -46,24 +81,34 @@ pub fn forward_propagation(
 
     let weighted_input = dot_product(&w_1, &input_image);
 
-    let z_1 = matrix_addition(&weighted_input, &_b_1);
+    let z_1 = linear_op(ADD, &weighted_input, &_b_1);
 
     let activation_1 = relu(&z_1);
 
     let weighted_l1 = dot_product(&w_2, &activation_1);
 
-    let z_2 = matrix_addition(&weighted_l1, &b_2);
+    let z_2 = linear_op(ADD, &weighted_l1, &b_2);
 
     let activation_2 = softmax(&z_2);
 
     (z_1, activation_1, z_2, activation_2)
 }
 
-#[allow(dead_code)]
-pub fn backward_propagation(
+pub fn back_propagation(
     network_params: NetworkParams,
-    w_2: Vec<Vec<f64>>,
-    labels: Vec<Vec<f64>>,
+    _w_2: Vec<Vec<f64>>,
+    _labels: Vec<Vec<f64>>,
 ) {
+    let (z_1, activation_1, z_2, activation_2) = network_params;
+    // let output_labels = to_output_labels(rows_len, cols_len, &labels);
+
+    let expected_labels = transform_labels_to_network_output(&_labels);
+
+    let delta_z_2 = linear_op(SUBTRACT, &activation_2, &expected_labels);
+
+    let transposed_a_1 = transpose(&activation_1);
+
+    let delta_w_2 = dot_product(&delta_z_2, &transposed_a_1);
+
     todo!()
 }
