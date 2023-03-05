1d5d3326e6ba9ca5bc86d60b2f72768bb6bb5871
YogirajT <tawdeyogiraj@yahoo.com>
2023-03-06 02:23:43 +0530

    WIP: Operator Overload test

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
 pub mod common;
+use std::ops::Add;
+use std::ops::{Deref, DerefMut};
+use std::vec::Vec;
+
+struct NumpyVec<T>(Vec<T>);
 
 #[macro_export]
 macro_rules! save_to_file {
@@ -11,4 +16,49 @@ macro_rules! save_to_file {
             };
         }
     };
+}
+
+// operator overloading
+impl<T> Deref for NumpyVec<T> {
+    type Target = Vec<T>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> DerefMut for NumpyVec<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<T: Add<Output = T> + Copy + 'static> Add<NumpyVec<NumpyVec<T>>> for NumpyVec<NumpyVec<T>>
+where
+    &'static T: std::ops::Add<T>,
+{
+    type Output = Vec<T>;
+
+    fn add(self, _rhs: NumpyVec<NumpyVec<T>>) -> std::vec::Vec<T> {
+        assert!(
+            self.len() == _rhs.len()
+                && (self.first().unwrap().len() == _rhs.first().unwrap().len()
+                    || _rhs.first().unwrap().len() == 1),
+            "Matrix dimensions must match for addition."
+        );
+
+        let result = Vec::new();
+
+        for (i, row) in self.iter().enumerate() {
+            let mut new_row = Vec::new();
+            for (j, cell) in row.iter().enumerate() {
+                if _rhs.first().unwrap().len() == 1 {
+                    new_row.push(*cell + _rhs[i][0]);
+                } else {
+                    new_row.push(*cell + _rhs[i][j]);
+                }
+            }
+        }
+        result
+    }
 }
